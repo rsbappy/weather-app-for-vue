8471eac15e2d2204888d90d4493450287bfdeeb3
rsbappy <bappykha226@gmail.com>
2023-04-10 23:34:20 +0600

    weather app ui design

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,125 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="container">
+    <div class="wrap">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+
+      <!-- search box for laction -->
+      <div class="search-box">
+        <input type="text" placeholder="Search ...." class="search-bar">
+      </div>
+
+      <!-- weather information -->
+      <div class="weather-info">
+        <div class="location-box">
+          <div class="location">Dhaka</div>
+          <div class="date">17-3-23</div>
+        </div>
+        <div class="weather-box">
+          <div class="temp">22 c</div>
+          <div class="weather">dsdsfs</div>
+
+        </div>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700;900&display=swap');
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Montserrat', sans-serif;}
+  .container{
+    background-image: url("./assets/weather-bg.jpg");
+    background-size: cover;
+    background-position: center;
+    transition: 0.4s;
+    width: 375px;
+    margin: 0 auto;
+    border-radius: 25px;
+    margin-top: 50px;
+    box-shadow: 0px 0px 30px #00000065;
+
+  }
+  .wrap{
+    height: 600px;
+    padding: 25px;
+    border-radius: 25px;
+    background-image: linear-gradient(to bottom, rgba(0,0,0,0.15), rgba(0,0,0,0.4));
+
+
+  }
+  .search-box .search-bar {
+    display: block;
+    width: 100%;
+    padding: 15px;
+    color: #313131;
+    font-size: 20px;
+    appearance: none;
+    border: none;
+    outline: none;
+    background: none;
+    background-color: rgba(255, 255, 255, 0.5);
+    box-shadow: 0px 0px 8px rgba(0,0,0,0.25);
+    border-radius: 10px;
+    transition: 0.4s;
+  }
+  .search-box .search-bar:focus{
+    box-shadow: 0px 0px 16px rgba(0,0,0,0.25);
+    background-color: rgba(255, 255, 255, 0.75);
+
+
+
+  }
+  .location-box .location {
+    color: white;
+    font-size: 32px;
+    font-weight: 700px;
+    font-style: italic;
+    text-align: center;
+    margin-top: 30px;
+
+
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+
+  .location-box .date {
+    color: white;
+    font-size: 20px;
+    font-weight: 300;
+    text-align: center;
+  }
+  .weather-box{
+    text-align: center;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  .weather-box .temp {
+    display: inline-block;
+    padding: 10px 25px;
+    color: white;
+    font-size: 100px;
+    font-weight: 900;
+    text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+    background-color: rgba(255, 255, 255, 0.5);
+    border-radius: 16px;
+    margin: 30px 0px;
+    box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+    
   }
-}
+  .weather-box .weather{
+    color: white;
+    font-size: 48px;
+    font-weight: 700;
+    font-style: italic;
+    text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+
+  }
 </style>
